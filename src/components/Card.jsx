85b1e4b35949c693231ee8bf929e1aac2f010c39
Leshista <a.leshista@gmail.com>
2024-04-24 16:47:09 +0300

    added responsiveness to index page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,6 +9,7 @@ const Card = () => {
     const [dotsColor, setDotsColor] = useState('#161616')
     const toggleDropdownHandler = () => {
         setDropdownIsOpen(!dropdownIsOpen)
+        !dropdownIsOpen ? setDotsColor('#595959') : setDotsColor('#161616')
     }
     const dropdownHideHandler = () => {
         setDropdownIsOpen(false)
@@ -25,16 +26,18 @@ const Card = () => {
             <div className={classes.card} onMouseLeave={dropdownHideHandler}>
                 <img src={me} className={classes.card__image}></img>
                 <div className={classes.cardRight__wrapper}>
-                    <div className={classes.cardRight__topWrapper}>
+                <div className={classes.cardRight__topWrapper}>
+                    <div className={classes.card__titleWrapper}>
                         <h2 className={classes.card__title}>123</h2>
+                        <div className={classes.dropdown__wrapper}>
+                            <div className={classes.dots} onClick={toggleDropdownHandler} onMouseEnter={dotsHoverOnHandler} onMouseLeave={dotsHoverOffHandler}><Dots fill={dotsColor} size={screen.width > 800 ? 24 : 16} /></div>
+                            {dropdownIsOpen && <Dropdown isArchived={false} />}
+                        </div>
+                    </div>
                         <h3 className={classes.card__subtitle}>123</h3>
                     </div>
                     <div className={classes.cardRightm__bottomWrapper}>
                         <p className={classes.card__city}>123</p>
-                    </div>
-                    <div className={classes.dropdown__wrapper}>
-                    <div className={classes.dots} onClick={toggleDropdownHandler} onMouseEnter={dotsHoverOnHandler} onMouseLeave={dotsHoverOffHandler}><Dots fill={dotsColor} /></div>
-                    {dropdownIsOpen && <Dropdown isArchived={false} />}
                     </div>
                 </div>
             </div>
